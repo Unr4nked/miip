2443a200fd87c752955d5524bbdbf42c1175f9b2
Fcryptx <157066678+Fcryptx@users.noreply.github.com>
2026-02-14 20:02:59 -0500

    Add files via upload

--- miiip.sh
+++ miiip.sh
@@ -1,30 +1,31 @@
-#!/bin/bash
+}#!/bin/bash
 clear
-         out=$(curl https://ippublica.net -s | grep "h3" | tr '<>/{' ' ' | awk '{print $2}')
-         animate_1='      / /'
-         animate_2='     / /'
-         animate_3='     \ \'
-         animate_4='      \ \'
-         animate_5='       \ \'
-         animate_6='       / /'
-         animate_7='      / /'
-         animate_8='     //|\'
+out=$(curl https://ippublica.net -s | grep "h3" | tr '<>/{' ' ' | awk '{print $2}')
+ip=$(echo $out)
+animate_1='      / /'
+animate_2='     / /'
+animate_3='     \ \'
+animate_4='      \ \'
+animate_5='       \ \'
+animate_6='       / /'
+animate_7='      / /'
+animate_8='     //|\'
          for i in {1};do
-                 echo $animate_1
+                 echo -e "\t$animate_1"
                  sleep 0.1
-                 echo $animate_2
+                 echo -e "\t$animate_2"
                  sleep 0.1
-                 echo $animate_3
+                 echo -e "\t$animate_3"
                  sleep 0.1
-                 echo $animate_4
+                 echo -e "\t$animate_4"
                  sleep 0.2
-                 echo $animate_5
+                 echo -e "\t$animate_5"
                  sleep 0.2
-                 echo $animate_6
+                 echo -e "\t$animate_6"
                  sleep 0.34
-                 echo $animate_7
+                 echo -e "\t$animate_7"
                  sleep 0.34
-                 echo $animate_8
+                 echo -e "\t$animate_8"
                  sleep 1
-                 echo -e "\e[34m$out\e[0m"
+                 echo -e "\e[31m\n\t$ip\e[0m"
          done
