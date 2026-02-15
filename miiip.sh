a9af07a2b28aecfe6968ccc675169d2c5c6e2e60
Fcryptx <157066678+Fcryptx@users.noreply.github.com>
2026-02-14 20:06:18 -0500

    Add files via upload

--- miiip.sh
+++ miiip.sh
@@ -11,7 +11,7 @@ animate_6='       / /'
 animate_7='      / /'
 animate_8='     //|\'
          for i in {1};do
-                 echo -e "\t$animate_1"
+                 echo -e "\e[32m\t$animate_1"
                  sleep 0.1
                  echo -e "\t$animate_2"
                  sleep 0.1
@@ -25,7 +25,7 @@ animate_8='     //|\'
                  sleep 0.34
                  echo -e "\t$animate_7"
                  sleep 0.34
-                 echo -e "\t$animate_8"
+                 echo -e "\t$animate_8 \e[0m"
                  sleep 1
                  echo -e "\e[31m\n\t$ip\e[0m"
          done
